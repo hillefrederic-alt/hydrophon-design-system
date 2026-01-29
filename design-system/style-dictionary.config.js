031cff6199e8b09a045f42a71f4c5e74b0a36171
Frederic Hille <hillefrederic@gmail.com>
2026-01-29 14:01:36 +0100

    feat(07-04): add distribution platforms to Style Dictionary config
    - Add dist-css, dist-scss, dist-json platforms
    - Output to dist/tokens/ for NPM package distribution
    - Keep existing build/ outputs for development
    - SCSS platform added for Sass users

--- design-system/style-dictionary.config.js
+++ design-system/style-dictionary.config.js
@@ -3,6 +3,7 @@ import StyleDictionary from 'style-dictionary';
 export default {
   source: ['tokens/**/*.json'],
   platforms: {
+    // Development outputs
     css: {
       transformGroup: 'css',
       buildPath: 'build/css/',
@@ -19,6 +20,40 @@ export default {
     json: {
       transformGroup: 'js',
       buildPath: 'build/json/',
+      files: [
+        {
+          destination: 'tokens.json',
+          format: 'json/nested'
+        }
+      ]
+    },
+    // Distribution outputs
+    'dist-css': {
+      transformGroup: 'css',
+      buildPath: 'dist/tokens/css/',
+      files: [
+        {
+          destination: 'variables.css',
+          format: 'css/variables',
+          options: {
+            outputReferences: true
+          }
+        }
+      ]
+    },
+    'dist-scss': {
+      transformGroup: 'scss',
+      buildPath: 'dist/tokens/scss/',
+      files: [
+        {
+          destination: 'variables.scss',
+          format: 'scss/variables'
+        }
+      ]
+    },
+    'dist-json': {
+      transformGroup: 'js',
+      buildPath: 'dist/tokens/json/',
       files: [
         {
           destination: 'tokens.json',
